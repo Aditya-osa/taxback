e2252c6163e99399106c3b877d74689871a1964e
Aditya sahu <saditya23cs@student.mes.ac.in>
2026-07-09 11:16:42 +0530

    Update CORS configuration for local development

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://effulgent-lily-066668.netlify.app'],
+    'allowed_origins' => ['https://effulgent-lily-066668.netlify.app', 'http://localhost:3000', 'http://127.0.0.1:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
