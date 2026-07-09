cab909cfd8768d3f340f5c9afb353869d4db7c34
Aditya sahu <saditya23cs@student.mes.ac.in>
2026-07-09 12:24:40 +0530

    Allow CORS from live Netlify URL https://taaxlegal.netlify.app

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://effulgent-lily-066668.netlify.app', 'http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => ['https://effulgent-lily-066668.netlify.app', 'https://taaxlegal.netlify.app', 'http://localhost:3000', 'http://127.0.0.1:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
